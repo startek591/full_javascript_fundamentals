b4cb322dc07294455c4c2f439d012a869de1f618
jonathan_fambro <startek591@gmail.com>
2025-05-23 15:34:55 -0400

    Reserved Words

---
+++ Chapter_2/2.5_reserved_words.js
@@ -0,0 +1,11 @@
+/*
+as const export get null target void
+async continue extends if of this while
+await debugger false import return throw with
+break default finally in set true yield
+case delete for instanceof static try 
+catch do from let super typeof
+class else function new switch var
+
+enum implements interface package private protected public
+*/
