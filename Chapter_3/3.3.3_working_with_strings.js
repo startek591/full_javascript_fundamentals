722e4e431f776aceccded4e42f018d095b1b2549
jonathan_fambro <startek591@gmail.com>
2025-05-23 15:58:27 -0400

    Working With Strings

---
+++ Chapter_3/3.3.3_working_with_strings.js
@@ -0,0 +1,5 @@
+let msg = 'Hello, ' + 'world'; // Produces the string "Hello, world"
+let firstName = 'Jonathan';
+let greeting = 'Welcome to my blog, ' + ' ' + firstName;
+console.log(msg);
+console.log(greeting);
