40980f55abbc039de64ae1cb775fce61d5b1a70f
jonathan_fambro <startek591@gmail.com>
2025-05-23 15:42:19 -0400

    Dates and Times

---
+++ Chapter_3/3.2.6_dates_and_times.js
@@ -0,0 +1,8 @@
+let timestamp = Date.now();
+console.log(timestamp);
+let now = new Date();
+console.log(now);
+let ms = now.getTime();
+console.log(ms);
+let iso = now.toISOString();
+console.log(iso);
