6452fae297ab340ddde9597075c5866f5c88d9a6
jonathan_fambro <startek591@gmail.com>
2025-05-23 15:53:39 -0400

    String Literals

---
+++ Chapter_3/3.3.1_string_literals.js
@@ -0,0 +1,24 @@
+console.log(''); // The empty string: it has zero characters
+console.log('testing');
+console.log('3.14');
+console.log('name="myform"');
+console.log("Wouldn't you prefer O'Reilly's book?");
+console.log(
+  "t is the ratio of a circle's circumference to its radius"
+);
+console.log(`"She said 'hi'", he said.`);
+
+// A string representing 2 lines written on one line:
+console.log('two\nlines');
+
+// A one-line string written on 3 lines
+console.log(
+  'one\
+  long\
+  line'
+);
+
+// A two-line string written on two lines:
+console.log(
+  `the newline character at the end of this line is included literally in this string`
+);
